9968824dd87c56729168cbfbb19dff008b99fe9b
Hamamul Fauzi <hamamul.fauzi@gmail.com>
2020-02-20 14:12:54 +0700

    add unique to email, change auth key to 255 for facebook login

--- migrations/20200127124015-createUserTable.js
+++ migrations/20200127124015-createUserTable.js
@@ -6,36 +6,39 @@
  * Copyright (c) 2020 Bandung
  */
 
-'use strict';
+"use strict";
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('user', {
-      'id': {
+    return queryInterface.createTable("user", {
+      id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      'username': {
+      username: {
         allowNull: false,
         type: Sequelize.STRING(255),
         unique: true
       },
-      'password_hash': Sequelize.STRING(255),
-      'auth_key': Sequelize.STRING(32),
-      'password_reset_token': Sequelize.STRING(255),
-      'email': Sequelize.STRING(255),
-      'status': {
+      password_hash: Sequelize.STRING(255),
+      auth_key: Sequelize.STRING(255),
+      password_reset_token: Sequelize.STRING(255),
+      email: {
+        type: Sequelize.STRING(255),
+        unique: true
+      },
+      status: {
         type: Sequelize.SMALLINT,
         defaultValue: 10
       },
-      'created_at': Sequelize.INTEGER,
-      'updated_at': Sequelize.INTEGER
+      created_at: Sequelize.INTEGER,
+      updated_at: Sequelize.INTEGER
     });
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('user');
+    return queryInterface.dropTable("user");
   }
 };
