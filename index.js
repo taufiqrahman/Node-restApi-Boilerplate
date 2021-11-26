149f36e441616f987ef661291f3dd4a7842bd138
Taufiq Rahman <rahman.taufiq@gmail.com>
2021-11-26 14:28:19 +0700

    update

--- index.js
+++ index.js
@@ -11,10 +11,32 @@ const router = require('./routes/index')()
 const env = process.env.NODE_ENV || 'development';
 const config = require('./config/main')[env];
 const params = require('./config/params');
+const cors = require("cors")
 
 app = express();
+
+// ALLOW CORS
+app.use(cors());
+// SET CORS for PREFLIGHT OPTIONS
+app.options('*', cors());
+// app.use((req, res, next) => {
+//     console.log('request')
+//     res.header("Access-Control-Allow-Origin", '*');
+//     res.header("Content-Type", "application/json")
+//     res.header('Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT');
+//     res.header('Access-Control-Allow-Headers: X-Requested-With, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding, X-Auth-Token');
+//     if (req.method == "OPTIONS") {
+//         console.log('request option')
+//         res.status(200);
+//         res.send();
+//     } else {
+//         next();
+//     }
+
+// });
 app.use(bodyParser.json());
 app.use('/api', router);
+
 
 app.listen(config.serverport, () => {
     console.log('RESTful API server started on: ' + config.serverport);
